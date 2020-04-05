7e0d440aa291e76f0b13c1ef474301760d6d61f4
HahAmypage <13632439133@139.com>
2020-04-05 16:47:16 +0800

    创建springcloud配置服务

---
+++ config_model/src/main/java/com/davina/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.davina.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName ConfigServerApplication
+ * @Description TODO
+ * @Author Davina Chan
+ * @Date 2020/4/5 16:27
+ * @Version 1.0
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
